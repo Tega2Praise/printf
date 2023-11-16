c7796c61fa45d050d946bdc0de3c1aa6e5f350ed
Azbad <144432369+Azbad@users.noreply.github.com>
2023-11-16 09:56:21 +0100

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
+ * get_width - Function that calculates the width for printing
  * by Tega and Azeez
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
