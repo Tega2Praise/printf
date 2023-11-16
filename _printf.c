cbf7b0a297603db0b951e9b75006412046d10f68
Tega2Praise <egwabortega@gmail.com>
2023-11-16 11:48:29 -0800

    printf.c

--- _printf.c
+++ _printf.c
@@ -3,10 +3,9 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function that produces output according to a format
- * @format: String character.
- * by Tega and Azeez
- * Return: The number of printed characters.
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
