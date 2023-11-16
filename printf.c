985c9638cc6e401ac0db27414f1286658152f2f7
Azbad <144432369+Azbad@users.noreply.github.com>
2023-11-16 06:49:49 +0100

    Update printf.c

--- printf.c
+++ printf.c
@@ -6,7 +6,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * _printf - Printf function that produces output according to a format
  * @format: String character.
  * by Tega and Azeez
- * Return: The number of characters.
+ * Return: The number of printed characters.
  */
 int _printf(const char *format, ...)
 {
